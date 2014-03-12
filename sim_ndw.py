a747b27927c2601ae0804bf16b49ca8c4c807d1e
yyssyy11 <yyssyy11@gmail.com>
2014-03-12 20:04:20 +0800

    设置仿真环境和参数，节点个数和启动顺序等等；按照设定进行仿真，把仿真记录和结果输出到标准输出和log.txt中

---
+++ sim_ndw.py
@@ -0,0 +1,74 @@
+#! /usr/bin/python
+
+from random import *
+from TOSSIM import *
+from tinyos.tossim.TossimApp import *
+import sys
+
+n = NescApp()
+t = Tossim(n.variables.variables())
+r = t.radio()
+
+ff = open("log.txt", "w")
+
+topo = open("topo.txt", "r")
+for line in topo:
+  s = line.split()
+  if (len(s) > 0):
+    if (s[0] == "gain"):
+      r.add(int(s[1]), int(s[2]), float(s[3]))
+
+
+noise = open("meyer-heavy.txt", "r")
+for line in noise:
+  s = line.strip()
+  if s:
+    val = int(s)
+    for i in range(27):
+      t.getNode(i).addNoiseTraceReading(val)
+
+for i in range (1,27):
+  print "Creating noise model for ",i;
+  t.getNode(i).createNoiseModel()
+  t.getNode(i).bootAtTime(i * 11512170000 + 235423990)
+
+t.getNode(0).createNoiseModel()
+print "Creating noise model for 0";
+t.getNode(0).bootAtTime(i * 11512170000 + 235423990)
+
+
+m = t.getNode(0)
+v = m.getVariable("NdwC.energy_count")
+
+#t.addChannel("SINK", sys.stdout)
+#t.addChannel("BOOT", sys.stdout)
+#t.addChannel("BEACON", sys.stdout)
+#t.addChannel("PIT", sys.stdout)
+#t.addChannel("FIB", sys.stdout)
+#t.addChannel("CS", sys.stdout)
+#t.addChannel("DATA", sys.stdout)
+#t.addChannel("REPO", sys.stdout)
+
+t.addChannel("SINK", ff)
+t.addChannel("BOOT", ff)
+#t.addChannel("BEACON", ff)
+#t.addChannel("PIT", ff)
+#t.addChannel("FIB", ff)
+#t.addChannel("CS", ff)
+#t.addChannel("DATA", ff)
+t.addChannel("REPO", ff)
+
+#t.runNextEvent()
+
+#while v.getData() > 3:
+#  t.runNextEvent()
+
+print "Runing..."
+
+for i in range(0, 1000000):
+  t.runNextEvent()
+
+while v.getData() > 3:
+  t.runNextEvent()
+
+print "-------------END----------------"
