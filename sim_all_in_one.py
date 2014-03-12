2c162aa3c7cb1b082517101861ae925040bb09e1
yyssyy11 <yyssyy11@gmail.com>
2014-03-12 20:07:02 +0800

    设置节点不同参数（Ndw.h中），编译仿真并统计结果

---
+++ sim_all_in_one.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+import sys
+
+head = open("Ndw.h", "rw")
+for line in head:
+  head_set = line.split()
+  if(head_set[0] == "TIMER_PERIOD_REPO"):
+    print head_set
+
+
+print "-------------END----------------"
