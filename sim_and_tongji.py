cc23dcc6d8105319ac3839a0f557e4e0e1ea6511
yyssyy11 <yyssyy11@gmail.com>
2014-03-12 20:06:02 +0800

    执行仿真并统计，方便运行

---
+++ sim_and_tongji.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+import sys
+import os
+
+os.system('make micaz sim')
+print "-----------------------------"
+import tsim
+import tongji
+
+
+print "-------------END----------------"
