a6e0b6fe88c4d53a451f5e043c87dec99953694a
yyssyy11 <yyssyy11@gmail.com>
2014-03-20 11:20:31 +0800

    仿真综合脚本

--- sim_all_in_one.py
+++ sim_all_in_one.py
@@ -26,6 +26,13 @@ def set_fun(name, val):
 
 name = "TIMER_PERIOD_REPO"
 val = [5,10,20,50,100,150,200]
+
+cs_name = "CS_STALE_COUNT"
+cs_val = [0,1,2,3,4,5]
+
+pit_name = "PIT_STALE_COUNT"
+pit_val = [0,1,2,3,4,5]
+
 ISOTIMEFORMAT='%Y-%m-%d %X'
 
 def time_sim():
@@ -44,8 +51,43 @@ def time_sim():
 		tongji_item = sim_tongji.tongji()
 		res_file.write(tongji_item + '\n')
 
+def cs_sim():
+	res_file = open("cs_sim_result.txt", "w")
+	res_file.write('\ncs_sim_result.txt\n'+time.strftime(ISOTIMEFORMAT, time.localtime(time.time()))+'\n\n')
+	res_file.write("CS缓存时间（s）	发送请求数量	收到回应数量	丢包率		接收时延（大致/ms）" + '\n')
+	print ("CS缓存时间（s）	发送请求数量	收到回应数量	丢包率		接收时延（大致/ms）" + '\n')
+	for i in range(len(cs_val)):
+		set_fun(cs_name, cs_val[i])
+		os.system('make micaz sim > /dev/null 2>&1')
+		res_file.write(str(cs_val[i]) + '\t\t\t\t')
+		
+		#sim_ndw.sim()
+		os.system('python sim_ndw.py')
+		print (str(cs_val[i]) + '\t\t\t\t'),
+		tongji_item = sim_tongji.tongji()
+		res_file.write(tongji_item + '\n')
+
+def pit_sim():
+	res_file = open("pit_sim_result.txt", "w")
+	res_file.write('\npit_sim_result.txt\n'+time.strftime(ISOTIMEFORMAT, time.localtime(time.time()))+'\n\n')
+	res_file.write("PIT缓存时间（s）	发送请求数量	收到回应数量	丢包率		接收时延（大致/ms）" + '\n')
+	print ("PIT缓存时间（s）	发送请求数量	收到回应数量	丢包率		接收时延（大致/ms）" + '\n')
+	for i in range(len(pit_val)):
+		set_fun(pit_name, pit_val[i])
+		os.system('make micaz sim > /dev/null 2>&1')
+		res_file.write(str(pit_val[i]) + '\t\t\t\t')
+		
+		#sim_ndw.sim()
+		os.system('python sim_ndw.py')
+		print (str(pit_val[i]) + '\t\t\t\t'),
+		tongji_item = sim_tongji.tongji()
+		res_file.write(tongji_item + '\n')
 
 
 if __name__ == '__main__':
-	time_sim()
+	#time_sim()
+	#cs_sim()
+	pit_sim()
+
+
 	print "----------ALL END--------------"
